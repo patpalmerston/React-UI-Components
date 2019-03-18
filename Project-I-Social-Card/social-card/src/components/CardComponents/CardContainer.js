7c221f5d4f179c150df01fba751effe01f68af41
patpalmerston <patpalmerston@gmail.com>
2019-03-18 15:28:38 -0700

    pain day

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Card.css';
+
+import CardBanner from './CardBanner.js';
+import CardContent from './CardContent';
+
+const CardContainer = () => {
+  return (
+    
+    <a href='https://www.reactjs.org'>
+    <div className='cards-container'>
+    <CardBanner />
+    <CardContent />
+    </div>
+    </a>
+    
+  );
+};
+
+export default CardContainer;
+
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContent = () => {
+  return (
+  <div class='header-content'><p>Let's learn React by building simple interfaces with components. Dont try to overthink it, just keep it simple and have fun. ONve you feel comfortable using components you ar well on your way to mastering React!</p>
+  </div>
+    );
+};
+
+export default HeaderContent; 
