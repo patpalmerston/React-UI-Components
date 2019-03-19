a297a98b559d480accc762696b164f2f9e668814
patpalmerston <patpalmerston@gmail.com>
2019-03-19 15:16:09 -0700

    mvp

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import './App.css';
+// import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className='calculator'>
+
+      <CalculatorDisplay text='0' />
+      <ActionButton text='clear'/>
+      <NumberButton text='÷' />
+      <NumberButton buttonStyle='white' text='1' />
+      <NumberButton buttonStyle='white' text='2' />
+      <NumberButton buttonStyle='white' text='3' />
+      <NumberButton text='x' />
+      <NumberButton buttonStyle='white' text='4' />
+      <NumberButton buttonStyle='white' text='5' />
+      <NumberButton buttonStyle='white' text='6' />
+      <NumberButton text='-' />
+      <NumberButton buttonStyle='white' text='7' />
+      <NumberButton buttonStyle='white' text='8' />
+      <NumberButton buttonStyle='white' text='9' />
+      <NumberButton text='+' />
+      <ActionButton text='0'/>
+      <NumberButton text='=' />
+      
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Button.css';
+
+
+
+
+
+const NumberButton = props => <button className={`numberButtonStyles ${props.buttonStyle}`}>{props.text}</button>;
+
+NumberButton.defaultProps = {
+  buttonStyle: "red",
+  fontStyle: 'fontWhite'
+};
+
+
+export default NumberButton;
